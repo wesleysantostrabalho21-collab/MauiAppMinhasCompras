7d999b15d004a43cdaa4afd111e351875b61bbc2
oweas <oweas@WES>
2026-03-01 12:18:12 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using MauiAppMinhasCompras.Models;
+
+
+namespace MauiAppMinhasCompras.Helpers
+{
+    public class SQLiteDatabaseHelper
+    {
+readonly SQLiteAsyncConnection _connection;
+
+        public SQLiteDatabaseHelper() { }
+    }
+}
