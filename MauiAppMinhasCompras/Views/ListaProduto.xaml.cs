838ee9a84eefd86c23a95db341dabfec9a489ec3
oweas <wesleysantostrabalho21@gmail.com>
2026-03-08 14:47:33 -0300

    Agenda 03, Definindo o front-end e fazer o INSERT no SQLite

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -6,4 +6,15 @@ public partial class ListaProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Navigation.PushAsync(new Views.NovoProduto());
+		} catch (Exception ex)
+		{
+			DisplayAlert("ops", ex.Message, "OK");
+		}
+    }
 }
